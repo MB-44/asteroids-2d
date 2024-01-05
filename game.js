19ecfc8645009e829c84057f6c1aeb6ec5a6b4cf
1 <menathlakvindub@gmail.com>
2024-01-05 23:40:30 +0530

    omg finally created player | that freaking arc

--- game.js
+++ game.js
@@ -16,10 +16,19 @@ class Player {
     }
 
     draw() {
+        c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+        c.fillStyle = "red";
+        c.fill();
+
         // c.fillStyle = "red";
         // c.fillRect(this.position.x, this.position.y, 100, 100);
-        c.moveTo(this.position.x, this.position.y);
-        c.lineTo()
+        c.moveTo(this.position.x + 30, this.position.y);
+        c.lineTo(this.position.x - 10, this.position.y - 10);
+        c.lineTo(this.position.x - 10, this.position.y + 10);
+        c.closePath();
+
+        c.strokeStyle = "white";
+        c.stroke();
     }
 }
 
