6455c81ccda1c72006bc202e0199b642dcdb2c63
1 <menathlakvindub@gmail.com>
2024-01-05 23:08:01 +0530

    setup done:

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
+// setup
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
+c.fillStyle = "black";
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+// player
