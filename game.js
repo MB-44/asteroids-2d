32b3414d009960100c94fec5960521ca664b4c1f
1 <menathlakvindub@gmail.com>
2024-01-05 23:31:47 +0530

    creating player

--- game.js
+++ game.js
@@ -2,10 +2,32 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 c.fillStyle = "black";
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 // player
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position; 
+        this.velocity = velocity
+    }
+
+    draw() {
+        // c.fillStyle = "red";
+        // c.fillRect(this.position.x, this.position.y, 100, 100);
+        c.moveTo(this.position.x, this.position.y);
+        c.lineTo()
+    }
+}
+
+const player = new Player({
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0}
+});
+
+player.draw();
+
+console.log(player);
