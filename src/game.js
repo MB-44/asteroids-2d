372ad6bbae8f7aa8e75d3513dc9f8edcb3df7f8d
1 <menathlakvindub@gmail.com>
2024-01-06 09:17:58 +0530

    hw the hell do i rotate this thing

--- src/game.js
+++ src/game.js
@@ -5,21 +5,22 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-c.fillStyle = "black";
-c.fillRect(0, 0, canvas.width, canvas.height);
-
 // player
 class Player {
     constructor({ position, velocity }) {
         this.position = position; 
-        this.velocity = velocity
+        this.velocity = velocity;
+        this.rotation = 0;
     }
 
     draw() {
+        c.save()
+        c.rotate()
         c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
         c.fillStyle = "red";
         c.fill();``
 
+        c.beginPath();
         c.moveTo(this.position.x + 30, this.position.y);
         c.lineTo(this.position.x - 10, this.position.y - 10);
         c.lineTo(this.position.x - 10, this.position.y + 10);
@@ -27,30 +28,87 @@ class Player {
 
         c.strokeStyle = "white";
         c.stroke();
+        c.restore();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 }
+
 
 const player = new Player({
     position: { x: canvas.width / 2, y: canvas.height / 2 },
     velocity: { x: 0, y: 0}
 });
 
-player.draw();
 
 // player movement
+
+const keys = {
+    ArrowUp : {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+
 function animate() {
     window.requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.update()
+
+    player.velocity.x = 0;
+    if (keys.ArrowUp.pressed) player.velocity.x = 1;
+
+    if (keys.ArrowRight.pressed) player.rotation += 0.01;
 }
+
+animate();
 
 window.addEventListener('keydown', (event) => {
     switch (event.code) {
         case 'ArrowUp':
+            keys.ArrowUp.pressed = true;
             break
         case 'ArrowDown':
+            keys.ArrowDown.pressed = true;
             break
         case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
             break
         case 'ArrowRight':
+            keys.ArrowRight.pressed = true;    
+            break
+    }
+});
+
+
+window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false;
+            break
+        case 'ArrowDown':
+            keys.ArrowDown.pressed = false;
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;    
             break
     }
 });
